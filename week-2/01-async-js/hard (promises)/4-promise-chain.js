0755ded9d8bcbb591bcd23d3d2564ff8bcdfc1fe
Shoaib Sohail <136471399+Shoaib-Sohail5@users.noreply.github.com>
2023-12-26 14:44:06 +0530

    Update 4-promise-chain.js

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -1,24 +1,44 @@
 /*
- * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
+ * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
  * Write a function that sequentially calls all 3 of these functions in order.
- * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
+ * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
+function waitOneSecond(a) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, a*1000);
+    });
 }
 
-function wait2(t) {
-
+function waitTwoSecond(b) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, b*1000);
+    });
 }
 
-function wait3(t) {
-
+function waitThreeSecond(c) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, c*1000);
+    });
 }
 
-function calculateTime(t1, t2, t3) {
+function calculateTime(a,b,c) {
 
+    let start = Date.now();
+     return waitOneSecond(a).then(() => 
+         waitTwoSecond(b)
+    ).then(() =>
+         waitThreeSecond(c)
+    ).then(() => 
+        Date.now()-start 
+     );
 }
-
+// calculateTime();
 module.exports = calculateTime;
